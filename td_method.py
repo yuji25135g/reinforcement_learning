9af1af3d59d848d1a5afecb499f45cd361b5e1d8
komatsu <komatsu@hitotocolabo.co.jp>
2022-12-12 16:11:51 +0900

    td法の実装

--- td_method.py
+++ td_method.py
@@ -12,41 +12,43 @@ from common.gridworld import GridWorld
 # ###エージェントクラス
 
 #%%
-class McAgent:
+class QLearningAgent:
     def __init__(self):
         self.gamma = 0.9
+        self.alpha = 0.8
         self.epsilon = 0.1
-        self.alpha = 0.1
         self.action_size = 4
 
         random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
         self.pi = defaultdict(lambda: random_actions)
+        self.b = defaultdict(lambda: random_actions)
         self.Q = defaultdict(lambda: 0)
-        self.memory = []
 
     def get_action(self, state):
-        action_probs = self.pi[state]
+        action_probs = self.b[state]
         actions = list(action_probs.keys())
         probs = list(action_probs.values())
         return np.random.choice(actions, p=probs)
 
-    def add(self, state, action, reward):
-        data = (state, action, reward)
-        self.memory.append(data)
+    # def add(self, state, action, reward):
+    #     data = (state, action, reward)
+    #     self.memory.append(data)
 
-    def reset(self):
-        self.memory.clear()
+    # def reset(self):
+    #     self.memory.clear()
 
-    def upgrade(self):
-        G = 0
-        for data in reversed(self.memory):
-            state, action, reward = data
-            G = self.gamma * G + reward
-            key = (state, action)
-            self.Q[key] += (G - self.Q[key]) * self.alpha
+    def update(self, state, action, reward, next_state, done):
+        if done:
+            next_q_max = 0
+        else:
+            next_qs = [self.Q[next_state, a] for a in range(self.action_size)]
+            next_q_max = max(next_qs)
+        
+        target = reward + self.gamma * next_q_max
+        self.Q[state, action] += (target - self.Q[state, action]) * self.alpha
 
-            self.pi[state] = greedy_probs(self.Q, state, self.epsilon)
-
+        self.pi[state] = greedy_probs(self.Q, state, epsilon=0)
+        self.b[state] = greedy_probs(self.Q, state, self.epsilon)
 
 #%%
 def greedy_probs(Q, state, epsilon=0, action_size=4):
@@ -61,21 +63,18 @@ def greedy_probs(Q, state, epsilon=0, action_size=4):
 
 #%%
 env = GridWorld()
-agent = McAgent()
+agent = QLearningAgent()
 
-episodes = 1000
+episodes = 10000
 for episode in range(episodes):
     state = env.reset()
-    agent.reset()
 
     while True:
         action = agent.get_action(state)
         next_state, reward, done = env.step(action)
 
-        agent.add(state, action, reward)
+        agent.update(state, action,  reward, next_state, done)
         if done:
-            print("done")
-            agent.upgrade()
             break
         state = next_state
 env.render_q(agent.Q)
